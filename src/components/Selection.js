9ea540aa83819b159947e92b3c0fc6b4da804740
IqbalZafar-07 <76274708+IqbalZafar-07@users.noreply.github.com>
2021-02-09 01:17:33 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
 const Selection = (props) =>{
-    const[background, setBackground] = useState({background : ""});
+    const[background, setBackground] = useState("");
     const {applyColor} = props;
     const change = (nextBackground) => {
-        setBackground({background : nextBackground.background})
+        setBackground(nextBackground.background)
     }
     return(
-        <div className="fit-Box" style={{background : background.background}} onClick={()=>{applyColor(change)}} >
+        <div className="fit-Box" style={{background : background}} onClick={()=>{applyColor(change)}} >
             <h2 className="subheading">Selection</h2>
         </div>
     )
