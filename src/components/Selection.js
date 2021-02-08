1d1a886886ba4bf47f0e86d32746f12887a748b7
IqbalZafar-07 <76274708+IqbalZafar-07@users.noreply.github.com>
2021-02-09 00:32:39 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
-
+import React, {useState} from 'react'
+const Selection = (props) =>{
+    const[background, setBackground] = useState({background : ""});
+    const {applyColor} = props;
+    const change = (nextBackground) => {
+        setBackground({background : nextBackground.background})
+    }
+    return(
+        <div className="fit-Box" style={background} onClick={()=>{applyColor(change)}} >
+            <h2 className="subheading">Selection</h2>
+        </div>
+    )
+}
+export default Selection;
